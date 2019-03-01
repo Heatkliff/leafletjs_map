3ab937132b5f9c5a100abb07d8d34364e5fddc52
Heathcliff <bahyce@gmail.com>
2019-03-02 01:37:59 +0200

    Add Map in index page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
+import leaflet from 'leaflet';
+import datatables from 'datatables.net';
 
 jQuery(function() {
     jQuery('body').css('color', 'blue');
+
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiaGVhdGtsaWZmIiwiYSI6ImNqc3Fvb3lqbjFhOHUzeW41OWI2MndvOTEifQ.pgDLYkmvTfauWZADQcg-Ew'
+    }).addTo(mymap);
 });
+
